20b7c694f7257f13b0a0f80e6cdadb6267bdfd38
unknown <kaushikadinith1996@gmail.com>
2022-03-27 14:34:22 +0530

    main component

--- ReactNativeTestProject/App.tsx
+++ ReactNativeTestProject/App.tsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
