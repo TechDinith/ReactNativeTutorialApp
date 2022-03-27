0c5ef9169638dc657aa79931c467848a272212b2
unknown <kaushikadinith1996@gmail.com>
2022-03-27 13:51:56 +0530

    all done

--- ReactNativeTestProject/App.tsx
+++ ReactNativeTestProject/App.tsx
@@ -1,18 +1,76 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
-  //states
+  //1
   // const [name, setName] = useState("Fred");
   // const [inputValue, setInputValue] = useState("");
 
-  //functions
   // const clickHandler = () => {
   //   setName("No-One");
   // };
 
+  //2
+  const [people, setPeople] = useState([
+    {
+      name: "fred",
+      id: "1",
+    },
+    {
+      name: "dead",
+      id: "2",
+    },
+    {
+      name: "zed",
+      id: "3",
+    },
+    {
+      name: "blood",
+      id: "4",
+    },
+    {
+      name: "fred",
+      id: "5",
+    },
+    {
+      name: "dead",
+      id: "6",
+    },
+    {
+      name: "zed",
+      id: "7",
+    },
+    {
+      name: "blood",
+      id: "8",
+    },
+  ]);
+
+  const pressHandler = (id: string) => {
+    console.log("id", id);
+
+    //oneway
+    // setPeople((oldPeople) => {
+    //   return oldPeople.filter(
+    //     (person: { name: string; id: string }) => person.id != id
+    //   );
+    // });
+    //anothorway
+    setPeople([...people.filter((person: any) => person.id != id)]);
+    console.log("people", people);
+  };
   return (
     <View style={styles.container}>
+      {/* 1 */}
       {/* <View>
         <Text>Hellow, {name}</Text>
 
@@ -28,6 +86,29 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <Button onPress={clickHandler} title="Update Title" />
       </View> */}
+
+      {/* 2 */}
+      {/* <ScrollView>
+        {people.map((item, id) => (
+          <View key={id + 1}>
+            <Text style={styles.item}>
+              {item.name} {id + 1}
+            </Text>
+          </View>
+        ))}
+      </ScrollView> */}
+
+      {/* 3 */}
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -36,9 +117,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    //1
+    // alignItems: "center",
+    // justifyContent: "center",
+
+    // 2
+    padding: 40,
+    paddingHorizontal: 20,
   },
+  //1
   // buttonContainer: {
   //   marginTop: 20,
   // },
@@ -49,4 +136,15 @@ const styles = StyleSheet.create({
   //   margin: 10,
   //   width: 200,
   // },
+
+  //2
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+
+    // 3
+    marginHorizontal: 10,
+  },
 });
