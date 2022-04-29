0a2a5b9569b506cd7a17222dc9191563adc5b289
unknown <kaushikadinith1996@gmail.com>
2022-04-29 15:41:02 +0530

    all done

--- ReactNativeTestProject/App.tsx
+++ ReactNativeTestProject/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import TodoApp from "./component/todoApp";
 import store from "./_redux/store";
 
+//get out the apk
 export default function App() {
   return (
     <Provider store={store}>
